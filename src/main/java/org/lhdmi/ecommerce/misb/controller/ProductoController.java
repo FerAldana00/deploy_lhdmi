3d51463d5f74c73634da460ec617a6e6c5e42f79
FerAldana <ma.fe.al.ll.00@gmail.com>
2023-12-11 16:30:06 -0600

    Se eliminaron archivos

--- src/main/java/org/lhdmi/ecommerce/misb/controller/ProductoController.java
+++ src/main/java/org/lhdmi/ecommerce/misb/controller/ProductoController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 //CRUD - CREATE - READ - UPDATE - DELETE
 	//HTTP - POST - GET - PUT - DELETE
 //@CrossOrigin(origins="*")
-@CrossOrigin(origins = "")
+@CrossOrigin(origins = "url de frontend")
 @RestController
 @RequestMapping(path="/api/galeria/") //http://localhost:8080/api/galeria/
 public class ProductoController {
